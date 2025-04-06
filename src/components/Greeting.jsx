acb386a26eeb90b9062c1f455c25cce8624aaff5
ashlym1 <ashlym3@usf.edu>
2025-04-06 13:25:22 -0400

    Vite and React setup

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Greeting() {
+  return (
+    <div>
+      <h2>Hello from the Greeting Component!</h2>
+      <p>This is your first custom component in React.
+         Go and enjoy an iced matcha latte in this sunny weather ! </p>
+    </div>
+  );
+}
+
+export default Greeting;
